f806f924eaa02f23aebdc2c1fea3b637fd70dc3e
JankeerJinna23 <jankeer.jinna23@hotmail.com>
2019-07-11 16:49:28 +0300

    Committed

--- OmegaSchoolWebApi/Program.cs
+++ OmegaSchoolWebApi/Program.cs
@@ -20,5 +20,6 @@ namespace OmegaSchoolWebApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+	//test 1
     }
 }
